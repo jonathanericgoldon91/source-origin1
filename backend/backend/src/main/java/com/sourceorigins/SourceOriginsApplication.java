82ff9b27bc4242aa471961a63995e931b1570d64
jonathanericgoldon91 <jonathanericgoldon91@gmail.com>
2026-05-16 19:22:32 +0200

    feat: Point d'entée Spring Boot

---
+++ backend/backend/src/main/java/com/sourceorigins/SourceOriginsApplication.java
@@ -0,0 +1,11 @@
+package com.sourceorigins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourceOriginsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SourceOriginsApplication.class, args);
+    }
+}
