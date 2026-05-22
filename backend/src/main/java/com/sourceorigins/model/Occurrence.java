7657c32a7f938a2f245615fb1d233e79372e4a26
juniorvictoria26-max <juniorvictoria26@gmail.com>
2026-05-23 01:38:02 +0200

    Update Occurrence.java
    ajout des commentaires

--- backend/src/main/java/com/sourceorigins/model/Occurrence.java
+++ backend/src/main/java/com/sourceorigins/model/Occurrence.java
@@ -3,6 +3,7 @@ package com.sourceorigins.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 /**
  * Représente une occurrence (apparition) d'une image sur le web.
  */
@@ -10,10 +11,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Occurrence {
-    /** URL de la page ou l'image a ete trouvee */
+
+    /** URL de la page où l'image a été trouvée */
     private String url;
+
+    /** Titre de la page web */
     private String title;
+
+    /** Nom du site source (ex: Wikipedia, Facebook...) */
     private String source;
+
+    /** Date de publication de l'image */
     private String date;
+
+    /** URL de la miniature de l'image */
     private String thumbnailUrl;
 }
