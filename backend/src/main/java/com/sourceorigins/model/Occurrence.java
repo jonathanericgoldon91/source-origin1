6f9e5b532548563ec9947cb1d1130a4f5800fbb6
juniorvictoria26-max <juniorvictoria26@gmail.com>
2026-05-23 01:36:57 +0200

    Update Occurrence.java

--- backend/src/main/java/com/sourceorigins/model/Occurrence.java
+++ backend/src/main/java/com/sourceorigins/model/Occurrence.java
@@ -6,11 +6,11 @@ import lombok.NoArgsConstructor;
 /**
  * Représente une occurrence (apparition) d'une image sur le web.
  */
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Occurrence {
+    /** URL de la page ou l'image a ete trouvee */
     private String url;
     private String title;
     private String source;
